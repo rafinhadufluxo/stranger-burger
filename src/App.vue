c1edd4982cc0e2f959c41ef14a8484f2a337446d
Rafa <arruda.rafa.ra@gmail.com>
2023-01-04 14:54:22 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <NavBar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  data: function () {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Stranger Burges",
+    };
+  },
+  components: {
+    NavBar,
+    Footer,
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
